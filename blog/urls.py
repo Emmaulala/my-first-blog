5b53ccf9ce946134f64fcf7463b95340258eb71a
Emma Cheng <emmaulala@gmail.com>
2024-04-02 08:02:25 +0800

    Added views to create/edit blog post inside the site

--- blog/urls.py
+++ blog/urls.py
@@ -6,4 +6,6 @@ urlpatterns=[
     #path(r'^$', views.post_list, name='post_list'), #原本path寫法用regex會有warning，應該是混用新舊版本django寫法
     re_path(r'^$', views.post_list, name='post_list'), #要用re_path寫法搭配regex才行
     re_path(r'^psot/(?P<pk>[0-9]+)/$', views.post_detail, name='post_detail'),
+    re_path(r'^post/new/$', views.post_new, name='post_new'),
+    re_path(r'^post/(?P<pk>[0-9]+)/edit/$', views.post_edit, name='post_edit'),
 ]  
